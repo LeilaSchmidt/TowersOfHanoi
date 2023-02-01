d70a46df7e66b57f0d84186a1387a52b4ae6a9bd
Leila Schmidt <le.schmidt@douglas.de>
2023-02-01 23:10:22 +0100

    implemented one new function (GetDisks) and one new method (DiskMovement)

--- TowersOfHanoi/Program.cs
+++ TowersOfHanoi/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace TowersOfHanoi
+﻿using System;
+
+namespace TowersOfHanoi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numOfDisks;
+            while (!int.TryParse(GetDisks(), out numOfDisks))
+            {
+                Console.WriteLine("This is not a number. Please enter a valid number.");
+            }
+
+            DiskMovement(numOfDisks, 'A', 'C', 'B');
+            Console.WriteLine("The stack is now sorted. ");
+        }
+
+        static string GetDisks()
+        {
+            Console.WriteLine("How many disks would you like to move? ");
+            return Console.ReadLine();
+        }
+
+        static void DiskMovement(int disks, char fromStack, char toStack, char helperStack)
+        {
+            if (disks <= 0)
+            {
+                return;
+            }
+            if (disks == 1)
+            {
+                Console.WriteLine("Move disk " + disks + " from " + fromStack + " to " + toStack);
+                return;
+            }
+            else
+            {
+                DiskMovement(disks - 1, fromStack, helperStack, toStack);
+                Console.WriteLine("Move disk " + disks + " from " + fromStack + " to " + toStack);
+                DiskMovement(disks - 1, helperStack, toStack, fromStack);
+            }
         }
     }
 }
